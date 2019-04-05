f78a4d657ecc96fe39a1e9cddfeae00924337955
Jason Bray <jason.bray@uon.edu.au>
2019-04-05 03:57:38 +0000

    added more to caeser cipher

--- main.c
+++ main.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 
-
 /* Caeser Cipher*/
-/* Encryption Function required*/
-//int encrypt(?arguments); k(key), ?string
+
+
+/* Encryption Function required:
+int encrypt(?arguments); k(key), ?string
+*/
+
 
 int main(){
     
-    char msg[26];  //?increase array size
-    
-    
-    int key;
+    char message[100];  //?array size
+        
+    int key, count;
     
     printf("Enter a message to encrypt: ");
-    
-    // ?scanf("%s", msg);
-    
+  //  scanf("%s", message);  ?whitespace
     printf("Enter key: ")
     scanf("%d", &key);
-    
-    /* if to ensure key is not a negative number when used with the modulus operator, %, as
-       the operator is not defined for a negative number */
-    if(key < 0){           
-        key = key + 26;
-    }
+    /* add +26 to key if negative to ensure it is a postive value */
     
     
-    //loop: ?for, while
-    //?switch or if needed
+    //loop: for() start of text until end of text
+    //function...
     
     //end loop
     
-    printf("The encrypted message is: "); // ?output encoded message
+    
+    //  printf("The encrypted message is: %s", message); ?whitespace 
     
     return 0;
 }
