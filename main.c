a41af3858d14159572b664cb47e9ab944fe8cf82
Jason Bray <jason.bray@uon.edu.au>
2019-04-12 04:58:40 +0000

    added rotation decipher without key

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main()
 {
 	char message[1000];                                        //array to store message 
-	int x, i, key;                                             //x for program option menu, key for key, i for array
+	int x, i, key, noKey;                                             //x for program option menu, key for key, i for array
 	
 	
 	printf("Enter message: \n");      
@@ -16,12 +16,16 @@ int main()
 	printf("1. Encrypt with a rotation cipher\n");
 	printf("2. Encrypt with substitution cipher\n");
 	printf("3. Decrypt a rotation cipher\n");
-	printf("4. Decrypt a substitution cipher\n");
+	printf("4. Decrypt a rotation cipher without key\n");
+	printf("5. Decrypt a substitution cipher\n");
+	printf("6. Decrypt a substitution cipher without key\n");
 	scanf("%d\n", &x);
 	
 	switch(x){
 	    //Rotation cipher encryption with key
 	    case 1: 
+	       printf("Enter key between 0 and 26: \n");
+	       scanf("%d", &key);
 	       for(i = 0; i < 1000 && message[i] != '\0' ; i++)     //loops until the message has been read then exits
 	           if(message[i] >= 'A' && message[i] <= 'Z'){      //this selects letters between A-Z
 	               message[i] = message[i] + key;               //performs the encryption with key
@@ -32,11 +36,16 @@ int main()
 	       printf("The encrypted message is: %s\n", message);
 	       break;
 
-
+        case 2:
+               printf("Enter key between 0 and 26: \n");
+	           scanf("%d", &key);
+        break;
 
 
         //Rotation ciphor decryption with key	     
-	    case 2:
+	    case 3:
+	       printf("Enter key between 0 and 26: \n");
+	       scanf("%d", &key);
 	       for(i = 0; i < 1000 && message[i] != '\0' ; i++)     //loops until message has been read then exits
 	           if(message[i] >= 'A' && message[i] <= 'Z'){      //this selects letters between A-Z
 	               message[i] = message[i] - key;               //performs the decryption with key
@@ -45,19 +54,41 @@ int main()
 	               }
                }
 	       printf("The decrypted message is: %s\n", message);
-	       break;
+	       break; 
 	       
+     //rotation ciphor without key
+	    case 4:	
+	    noKey = 1;
+	    while(noKey <26){
+	       for(i = 0; i < 1000 && message[i] != '\0' ; i++) {       //loops until the message has been read then exits
+	            if(message[i] >= 'A' && message[i] <= 'Z'){        //this selects letters between A-Z
+	                  message[i] = message[i] + noKey;             //tests the generated key
+    	               if(message[i] > 'Z')                       //if a letter moves outside of the data range A-Z
+	                       message[i] = message[i] - 'Z' + 'A' -1; //returns that letter back to the beginning of A-Z
+                }        
+            }
+                            
+                 
+                 printf("The encrypted message may be: %s\n", message);
+                 noKey++;
+        }
+
+
 	       
+	    break;
 	       
-	       
-	    case 3:
-	    
-	       break;
-	       
-	    case 4:
-	    
-	       break;
-	       
+	     
+	     
+	     
+        case 5:
+            printf("Enter key between 0 and 26: \n");
+            scanf("%d", &key);
+        
+        break;
+        
+        case 6:
+        break;
+        
 	    default:
 	       printf("Error\n");
 	}
